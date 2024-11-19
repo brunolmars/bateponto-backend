57078dee6975aed32765e84469d03a7585b4085c
bruno.lmars <bruno.lmarques@sempreceub.com>
2024-11-19 09:47:05 -0300

    sas

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const sequelize = require('./config/db');
 const Usuario = require('./models/usuario');
 const Ponto = require('./models/ponto');
 
+const cors = require('cors');
+
+
+app.use(express.json());
+app.use(cors());
 
 sequelize.sync({ alter: true })
     .then(() => {
@@ -16,23 +21,43 @@ sequelize.sync({ alter: true })
 });
 
 
-const airton = Usuario.create({ nome: "airton2", email: "airton2.junior@ceub.edu.br", login: "airton2", senha: "123"});
+//const airton = Usuario.create({ nome: "airton2", email: "airton2.junior@ceub.edu.br", login: "airton2", senha: "123"});
 
-app.get("/user/:id1-:id2", (req, res) => {
-    console.log(req.params);
-});
-
-app.post("/user/:id1-:id2", (req, res) => {
-    res.send(req.params);
-});
-
-app.get("/teste", (req, res) => {
-    res.send("Resposta da rota /teste");
+// Rota que recupera todos os usuários do sistema
+app.get('/usuarios', async (req, res) => {
+    
+    const usuarios = await Usuario.findAll();
+    res.json(usuarios);
 });
 
 
-app.post("/rotapost", (req, res) => {
-    res.send("Retorno da rota usando o método post");
+// Rota que recupera um usuário específico
+app.get('/usuario/:id_usuario', async (req, res) => {
+    const id_usuario = req.params.id_usuario;
+
+    const usuario = await Usuario.findAll({
+        where: {
+            id_usuario: id_usuario
+        }
+    });
+
+    res.json(usuario);
+
+});
+
+
+// Rota que adiciona um usuário
+app.post('/usuario', async (req, res) => {
+    
+    const usuario = await Usuario.create({
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha,
+        login: req.body.login
+    });
+
+
+    res.json(usuario);
 });
 
 
